05128eef2c3c5735845eae3f01c1a41e6223626c
lvbowen <1010606892@qq.com>
2020-11-02 21:43:35 +0800

    init

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+
+const FourZeroFour = () => {
+    return (
+        <em>自定义 404404404404</em>
+    )
+}
+
+export default FourZeroFour
---
+++ pages/posts/[id].js
@@ -0,0 +1,11 @@
+
+
+const id = () => {
+    return (
+        <div>
+            <h3>ididid</h3>
+        </div>
+    )
+}
+
+export default id;
